ca22e0daaacf78697682be7deef9c34d1977d8ab
dipansharora <dipansharora7@icloud.com>
2021-03-02 16:36:31 -0800

    added poem.txt and reading it from code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}",args);
+    // println!("\n{:?}\n",args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("\nSearching for {}",query);
+    println!("In file {}\n",filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("With text: \n{}", contents);
 }
